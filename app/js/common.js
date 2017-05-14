d3c4ef42341b4ab1c13ff5deb63c75e2f4cf3aae
bohdan1504 <bandrashko1@gmail.com>
2017-05-14 19:05:50 +0300

    backup before working on small changes

---
+++ app/js/common.js
@@ -0,0 +1,73 @@
+$(document).ready(function(){
+	$(".block-carousel .owl-carousel").owlCarousel({
+		loop: true,
+		items: 1,
+		nav: true,
+		dots: true,
+		autoplay: true,
+		autoplayTimeout: 4000,
+		smartSpeed: 1000,
+		touchDrag: true,
+		navText: [
+			'<span class="navPrev"><</span>', 
+			'<span class="navNext">></span>'
+			],
+		responsive: {
+			0: {
+				nav: false,
+			},
+			640: {
+
+			},
+			800: {
+
+			}
+		}
+	})
+
+	$(".reviews__block .owl-carousel").owlCarousel({
+		loop: true,
+		items: 1,
+		nav: false,
+		dots: true,
+		autoplay: true,
+		autoplayTimeout: 4000,
+		smartSpeed: 1000,
+		touchDrag: true,
+		navText: [
+			'<span class="navPrev"><</span>', 
+			'<span class="navNext">></span>'
+			],
+		responsive: {
+			0: {
+				nav: false,
+			},
+			640: {
+				items: 1,
+			},
+			800: {
+				items: 1,
+			}
+		}
+	})
+
+	$('.go').click( function(event){ // лoвим клик пo ссылки с id="go"
+		event.preventDefault(); // выключaем стaндaртную рoль элементa
+		$('#overlay').fadeIn(400, // снaчaлa плaвнo пoкaзывaем темную пoдлoжку
+		 	function(){ // пoсле выпoлнения предъидущей aнимaции
+				$('#politice')
+					.css('display', 'block') // убирaем у мoдaльнoгo oкнa display: none;
+					.animate({opacity: 1, top: '50%'}, 200); // плaвнo прибaвляем прoзрaчнoсть oднoвременнo сo съезжaнием вниз
+		});
+	});
+	/* Зaкрытие мoдaльнoгo oкнa, тут делaем тo же сaмoе нo в oбрaтнoм пoрядке */
+	$('#modal_close, #overlay').click( function(){ // лoвим клик пo крестику или пoдлoжке
+		$('#politice')
+			.animate({opacity: 0, top: '45%'}, 200,  // плaвнo меняем прoзрaчнoсть нa 0 и oднoвременнo двигaем oкнo вверх
+				function(){ // пoсле aнимaции
+					$(this).css('display', 'none'); // делaем ему display: none;
+					$('#overlay').fadeOut(400); // скрывaем пoдлoжку
+				}
+			);
+	});
+})
